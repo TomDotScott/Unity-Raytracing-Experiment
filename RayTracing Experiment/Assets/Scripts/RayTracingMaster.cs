2b025fc5e1a613a81dcfc1c0ed9820b95ef3cc05
tomdotscott <tom.scott45456@gmail.com>
2020-05-15 16:20:26 +0100

    RayTracingMaster Script
    OnRenderImage() is called by Unity as soon as the camera has finished rendering the meshes onto the screen.
    
    After this, the shader is Dispatched; this means that I am telling the GPU to get busy with a number of thread groups. The size and number of these thread groups can be set in 3 dimensions, which makes it easy to apply these compute shaders. The default thread group size in the unity template is (8, 8, 1), meaning one thread is spawned per 8x8 pixels. Finally the result of this is drawn on the screen using Graphics.Blit()

---
+++ RayTracing Experiment/Assets/Scripts/RayTracingMaster.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class RayTracingMaster : MonoBehaviour
+{
+    public ComputeShader rayTracingShader;
+    private RenderTexture target;
+
+    private void OnRenderImage(RenderTexture _source, RenderTexture _destination)
+    {
+        Render(_destination);
+    }
+
+    private void Render(RenderTexture _destination)
+    {
+        //Initalise the RenderTexture
+        InitRenderTexture();
+
+        //set the target
+        rayTracingShader.SetTexture(0, "Result", target);
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8);
+        rayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
+
+        //Blit the resulting texture to the screen
+        Graphics.Blit(target, _destination);
+    }
+
+    private void InitRenderTexture()
+    {
+        if(target == null || target.width != Screen.width || target.height != Screen.height)
+        {
+            if(target != null)
+            {
+                target.Release();
+            }
+            target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            target.enableRandomWrite = true;
+            target.Create();
+        }
+
+    }
+}
